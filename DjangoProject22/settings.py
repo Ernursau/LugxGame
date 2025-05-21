0629a12923134de77f9affb3bc93d5ca6692eb2e
Ernur Saulen <ernursaulen@gmail.com>
2025-05-21 08:34:38 +0500

    Fix ALLOWED_HOSTS for Render

--- DjangoProject22/settings.py
+++ DjangoProject22/settings.py
@@ -10,12 +10,13 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-fallback-dev-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-iqv6us4wuh)#($dqfvnb47i4omujztd(bv$iuz*+42way^um%#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
+
 
 # Application definition
 
