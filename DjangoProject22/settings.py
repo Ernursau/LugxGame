b598dd00ae26614caafa3cb82ccae791d1e1c828
Ernur Saulen <ernursaulen@gmail.com>
2025-05-21 08:38:47 +0500

    Fix ALLOWED_HOSTS for Render2

--- DjangoProject22/settings.py
+++ DjangoProject22/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-iqv6us4wuh)#($dqfvnb4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = "*"
+
 
 
 # Application definition
